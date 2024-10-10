23a1426843ee6e1584b50ac4ae08c66ae1dfbf0b
Mansurul Hakim <mansurulhakim2000@gmail.com>
2024-10-10 23:59:06 +0600

    User Area Added

---
+++ src/STPlatform/STPlatform.Web/Areas/User/Controllers/TeacherController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace STPlatform.Web.Areas.User.Controllers
+{
+    [Area("User")]
+    public class TeacherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
