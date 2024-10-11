c6f160193f18a763c9139666abc00fe715cdb852
Mansurul Hakim <mansurulhakim2000@gmail.com>
2024-10-12 00:01:03 +0600

    Added dashboard and ask question feature of student part. Registration part fixed

--- src/STPlatform/STPlatform.Web/Areas/User/Controllers/StudentController.cs
+++ src/STPlatform/STPlatform.Web/Areas/User/Controllers/StudentController.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace STPlatform.Web.Areas.User.Controllers
 {
     [Area("User")]
+    [Authorize]
     public class StudentController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+        public IActionResult AskQuestion()
         {
             return View();
         }
